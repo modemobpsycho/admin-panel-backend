01b5ddba449c1eb16e39cbfb598d1cc7ec476a87
Vadim Taratuta <hawaiianwork@mail.ru>
2024-07-18 21:22:30 +0300

    other

---
+++ config/config.ts
@@ -0,0 +1,9 @@
+import { config } from "dotenv"
+
+config()
+
+export const MyConfig = {
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
+}
